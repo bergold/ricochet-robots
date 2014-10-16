872a9465b9030f98dc1739924eced0f895df5867
bergold <emil.bergold@outlook.com>
2014-10-16 21:20:58 +0200

    use PORT from env, run zoned, add default handler

--- bin/server.dart
+++ bin/server.dart
@@ -13,28 +13,33 @@ part 'gamemanager.part.dart';
 void main() {
   
   var host = InternetAddress.LOOPBACK_IP_V4;
-  var port = 8080;
+  var port = Platform.environment.containsKey('PORT') ? Platform.environment['PORT'] : 5000;
   
   SocketManager socketManager = new SocketManager();
   HttpManager httpManager = new HttpManager();
   
-  HttpServer.bind(host, port).then((server) {
-    print('Server is running on \'http://${server.address.address}:${server.port}/\'');
-    
-    StreamController sc = new StreamController();
-    sc.stream.transform(new WebSocketTransformer()).listen((WebSocket ws) {
-      socketManager.handler(ws);
+  runZoned(() {
+    HttpServer.bind(host, port).then((server) {
+      print('Server is running on \'http://${server.address.address}:${server.port}/\'');
+      
+      StreamController sc = new StreamController();
+      sc.stream.transform(new WebSocketTransformer()).listen((WebSocket ws) {
+        socketManager.handler(ws);
+      });
+      
+      server.listen((HttpRequest request) {
+        if (request.uri.path == '/') {
+          request.response.write('Hello world from Dart on Heroku');
+          request.response.close();
+        } else if (request.uri.path == '/ws') {
+          sc.add(request);
+        } else {
+          httpManager.sendError(HttpStatus.NOT_FOUND, request);
+        }
+      });
+      
     });
-    
-    server.listen((HttpRequest request) {
-      if (request.uri.path == '/ws') {
-        sc.add(request);
-      } else {
-        httpManager.sendError(HttpStatus.NOT_FOUND, request);
-      }
-    });
-    
-  });
+  }, onError: (e, stackTrace) => print('HttpServer Error! $e $stackTrace'));
   
 }
 
