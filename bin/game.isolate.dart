8f5154ed47ec70a337014a71e667e258b7e2b2af
Emil <emil.bergold@outlook.com>
2014-11-15 15:16:41 +0100

    need to import messages lib

--- bin/game.isolate.dart
+++ bin/game.isolate.dart
@@ -2,13 +2,15 @@ library ricochetrobots_backend.gameisolate;
 
 import 'dart:isolate';
 
+import 'package:ricochetrobots/messages.dart';
+
 void main(List<String> args, SendPort sendPort) {
   var receivePort = new ReceivePort();
   sendPort.send(receivePort.sendPort);
   
   var adminId = args[0];
   
-  receivePort.listen((msg) {
+  receivePort.listen((MessageBase msg) {
     // [Todo] got msg
     print("got msg");
   });
