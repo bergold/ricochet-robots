f5831b087f7bd4d788d2be884668b3e41f353f08
Emil <emil.bergold@outlook.com>
2014-11-18 22:58:03 +0100

    run zoned, prefix prints

--- lib/analytics.dart
+++ lib/analytics.dart
@@ -1,5 +1,6 @@
 library ricochetrobots.analytics;
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -10,28 +11,29 @@ var _token = Platform.environment['ANALYTICS_TOKEN'];
 /// 
 /// This function sends a http post request to the analytics logger.
 void trigger(String event, [Map data = null]) {
-  if (_url == null) {
-    print('Warning: $event could not be triggered: No url specified.');
-    return;
-  }
-  if (_token == null) {
-    print('Warning: $event could not be triggered: No token specified.');
-    return;
-  }
-  
-  if (data == null) data = {};
-  
-  new HttpClient()..postUrl(_url)
-       .then((HttpClientRequest request) {
-         request.headers..add('Content-Type', 'application/json')
-                        ..add('X-Analytics-Event', event)
-                        ..add('X-Analytics-Token', _token);
-         request.write(JSON.encode(data));
-         return request.close();
-       })
-       .then((HttpClientResponse response) => response.drain(response))
-       .then((HttpClientResponse response) {
-         print('Event $event logged with status ${response.statusCode}');
-       });
-  
+  runZoned(() {
+    if (_url == null) {
+      print('Warning: Analytics event $event could not be triggered: No url specified.');
+      return;
+    }
+    if (_token == null) {
+      print('Warning: Analytics event $event could not be triggered: No token specified.');
+      return;
+    }
+    
+    if (data == null) data = {};
+    
+    new HttpClient()..postUrl(_url)
+         .then((HttpClientRequest request) {
+           request.headers..add('Content-Type', 'application/json')
+                          ..add('X-Analytics-Event', event)
+                          ..add('X-Analytics-Token', _token);
+           request.write(JSON.encode(data));
+           return request.close();
+         })
+         .then((HttpClientResponse response) => response.drain(response))
+         .then((HttpClientResponse response) {
+           print('Analytics event $event logged with status ${response.statusCode}');
+         });
+  }, onError: (e) => print('Analytics Uncaught Error: $e'));
 }
